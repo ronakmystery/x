0f7362ed8ff51189a2ebe2c34a79283886a33c2b
rmistr00UAFS <157536646+rmistr00UAFS@users.noreply.github.com>
2025-03-07 19:43:01 -0600

    cnn

--- AI/birds/src/settings.py
+++
@@ -1,7 +0,0 @@
-version = 'v1'
-name = "birds"
-path = fr'C:\Users\x\Documents\GitHub\x\AI'
-train = fr'{path}\{name}\train'
-test = fr'{path}\{name}\test'
-model = fr'{path}\{name}\{name}-{version}.pth'
-url = "http://192.168.1.213:8080/video"
